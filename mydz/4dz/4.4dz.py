1000d4f5a7ece6e44a637ad5b695de2c6343d97d
vladiqua <vlagoleg@gmail.com>
2026-04-22 14:49:04 +0300

    Update 4.4dz.py

--- mydz/4dz/4.4dz.py
+++ mydz/4dz/4.4dz.py
@@ -1,9 +1,9 @@
 color1 = input()
 color2 = input()
 
-primary_colors = {'красный', 'синий', 'желтый'}
+colors = {'красный', 'синий', 'желтый'}
 
-if color1 not in primary_colors or color2 not in primary_colors:
+if color1 not in colors or color2 not in colors:
     print("ошибка цвета")
 else:
     if color1 == color2:
